a83323d782c5be5e95432700229dafe732990ddd
boboghumah <boboghumah@gmail.com>
2018-09-07 19:14:50 +0100

    Add automation tests for google search

---
+++ README.md
@@ -0,0 +1,28 @@
+# Relayr Assessment
+
+## Getting started
+
+#### Requirements [Important]
++ This project uses the [Nightwatch.js](http://nightwatchjs.org/) testing framework
++ You will need to have the Java Development Kit (JDK) installed. The minimum required version is 7. 
+You can check this by running `java -version` from the command line.
+
+
+#### Getting Getting the source code
+To get started, clone the repository using the commands
+
+```bash
+git clone https://github.com/boboghumah/relayr-assessment.git
+```
+
+#### Install test dependencies
+```bash
+npm install
+```
+
+#### Running the tests
+```bash
+npm test
+```
+
+
---
+++ tests/search.test.js
@@ -0,0 +1,25 @@
+gmodule.exports = {
+    'Validate that the search field autosuggestion feature displays matching keywords': function (browser) {
+        browser
+            .url('https://google.com/')
+            .waitForElementVisible('body', 1000)
+            .waitForElementVisible('input.gsfi', 1000)
+            .setValue('input.gsfi', 'quality assurance')
+            .waitForElementVisible('ul[role=listbox]', 1000)
+            .assert.containsText('ul[role=listbox]', 'quality assurance')
+        },
+        
+        'Validate that Search result displays relevant search result and a correct page title': function (browser) {
+            browser
+            .url('https://google.com/')
+            .waitForElementVisible('body', 1000)
+            .waitForElementVisible('input.gsfi', 1000)
+            .setValue('input.gsfi', 'quality assurance')
+            .keys(browser.Keys.ENTER, function(done) {
+                browser.pause(3000)
+                .assert.title('quality assurance - Google Search')
+                .assert.containsText('div#main', 'quality assurance');
+            })
+            .end();
+    }
+};
